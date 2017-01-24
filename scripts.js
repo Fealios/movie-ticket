3c6a2b036c83e91719c15abadb8033a32dae45ae
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-01-24 10:35:15 -0800

    create the html form needed for interection some userlogic testing consoles

--- scripts.js
+++ scripts.js
@@ -1,28 +1,42 @@
 
 
-function Ticket(name, age, time) = {
-  this.price = name;
-  this.name = age;
+function Ticket(name, age, time) {
+  this.name = name;
+  this.age = age;
   this.time = time;
+  this.price = 10;
 }
 
 Ticket.prototype.checkDate = function(date){
-  if
+  if (name.releaseDate < x) {
+    this.price - 1;
+  }
 }
 
-function Movie(date) = {
+Ticket.prototype.checkAge = function(age){
+  if(this.age < 15 || this.age > 50){
+    this.price - 1;
+  }
+}
+
+// Movie Selector =============================
+function Movie(date) {
   this.releaseDate = date;
 }
 
 var rogueOne = new Movie();
 var imitationGame = new Movie();
 var civilWar = new Movie();
+// end Movie Selector =========================
 
 $(document).ready(function(){
   $('#enter').submit(function(event){
     event.preventDefault();
     var name = $('#movieName').val();
-
-    var newTicket = new Ticket()
+    var age = parseInt($('#userAge').val());
+    var time = $('#userTime').val();
+    console.log(name);
+    console.log(age);
+    var newTicket = new Ticket(name, age, time);
   })
 })
