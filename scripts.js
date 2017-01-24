5bd78d1c5c1e4c316f56a49b5593f657760b6421
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-01-24 10:04:22 -0800

    Add some backend logic for the functionality of ticket counter including creating 2 object constructors for the movies themselves and customer add prototype function for customer objects to check date of movie release'

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+
+function Ticket(name, age, time) = {
+  this.price = name;
+  this.name = age;
+  this.time = time;
+}
+
+Ticket.prototype.checkDate = function(date){
+  if
+}
+
+function Movie(date) = {
+  this.releaseDate = date;
+}
+
+var rogueOne = new Movie();
+var imitationGame = new Movie();
+var civilWar = new Movie();
+
+$(document).ready(function(){
+  $('#enter').submit(function(event){
+    event.preventDefault();
+    var name = $('#movieName').val();
+
+    var newTicket = new Ticket()
+  })
+})
